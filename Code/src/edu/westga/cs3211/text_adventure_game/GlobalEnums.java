843494c35d01a0f1cb0a72ee09a809ec8253f301
James Bridges <jb00656@my.westga.edu>
2024-11-12 16:38:06 -0500

    Added GlobalEnums class
    Created a GlobalEnums class to hold relevant enums for the game.
    Currently contains enum 'ActionType' with types of actions available
    in the game (currently only holds 'MOVE')

---
+++ Code/src/edu/westga/cs3211/text_adventure_game/GlobalEnums.java
@@ -0,0 +1,17 @@
+package edu.westga.cs3211.text_adventure_game;
+
+/**
+ * This class serves as a container for various enums used throughout the program.
+ * 
+ * @author James Bridges
+ * @version Fall 2024
+ */
+public class GlobalEnums { 
+	
+	/**
+	 * The type of action that can be performed
+	 */
+	public enum ActionType {
+		MOVE
+	}
+}
