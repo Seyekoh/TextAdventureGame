14e23b94136ae3565e3536211cc00173ca7c69a1
James Bridges <jb00656@my.westga.edu>
2024-11-12 15:37:39 -0500

    Created UI based on the wireframe.
    Created and added the MainWindow.fxml code to display the UI that is
    shown in the wireframe.
    Updated the MainWindow.java code to reflect the added fxml elements to
    the view.

--- Code/src/edu/westga/cs3211/text_adventure_game/view/MainWindow.java
+++ Code/src/edu/westga/cs3211/text_adventure_game/view/MainWindow.java
@@ -1,10 +1,41 @@
 package edu.westga.cs3211.text_adventure_game.view;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+
 /** Codebehind for the Main Window of the application.
  * 
- * @author CS 3211
+ * @author James Bridges
  * @version Fall 2024
  */
 public class MainWindow {
+	
+	@FXML
+	private TextArea currentLocationDescriptionTextArea;
+	
+	@FXML
+	private TextArea availableActionDescriptionTextArea;
+	
+	@FXML
+	private ComboBox<String> actionsComboBox;
+	
+	@FXML
+	private Button takeActionButton;
+	
+	/**
+	 * Initializes the MainWindow.
+	 * 
+	 */
+	@FXML
+	public void initialize() {
+   // TODO document why this method is empty
+ }
+	
+	@FXML
+	private void onTakeActionButtonClicked() {
+   // TODO document why this method is empty
+ }
 
 }
