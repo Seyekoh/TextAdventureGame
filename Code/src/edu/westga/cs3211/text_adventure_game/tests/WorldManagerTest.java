fc8eeb59e7e6e58177fd6405ccbd2b9e568a3175
James Bridges <jb00656@my.westga.edu>
2024-11-14 23:14:50 -0500

    Added GameManager class and refactored project.
    Added GameManager class:
        fields:
            world:              the game world
            worldManager:       the manager for the game world
            player:             the player of the game
            currentLocation:    the current Location that the player is ActionTest
            isGameOverLose:     flag for if the player loses the game
            isGameOverWin:      flag for if the player wins the game
            gameOverMessage:    the message to be displayed at gameOver
    
    Refactored the following:
        WorldManager:           no longer had the startLcoation field
    
        Location:               removed the hazardDamage field
    
        GlobalEnums:            Added PIT to HazardType
    
        World:                  Added HazardManager reference
                                Moved startLocation reference here
    
    Added/updated test classes

---
+++ Code/src/edu/westga/cs3211/text_adventure_game/tests/WorldManagerTest.java
@@ -0,0 +1,34 @@
+package edu.westga.cs3211.text_adventure_game.tests;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs3211.text_adventure_game.WorldManager;
+
+/**
+ * Tests the WorldManager class
+ * 
+ * @author James Bridges
+ * @version Fall 2024
+ */
+public class WorldManagerTest {
+	
+	/**
+	 * Tests the constructor
+	 */
+	@Test
+	public void testConstructor() {
+		WorldManager manager = new WorldManager();
+		assertNotNull(manager);
+	}
+	
+	/**
+	 * Tests the getWorld method
+	 */
+	@Test
+	public void testGetWorld() {
+		WorldManager manager = new WorldManager();
+		assertNotNull(manager.getWorld());
+	}
+}
---
+++ Code/src/edu/westga/cs3211/text_adventure_game/viewModel/ViewModel.java
@@ -0,0 +1,114 @@
+package edu.westga.cs3211.text_adventure_game.viewModel;
+
+import edu.westga.cs3211.text_adventure_game.GameManager;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+/**
+ * The ViewModel for the MainWindow.
+ * 
+ * @author James Bridges
+ * @version Fall 2024
+ */
+public class ViewModel {
+	
+	private final GameManager gameManager;
+	
+	private StringProperty currentLocationDescription;
+	private StringProperty availableActionsDescriptions;
+	private StringProperty playerStatus;
+	private ListProperty<String> availableActions;
+	
+	/**
+	 * Constructs a new ViewModel object.
+	 */
+	public ViewModel() {
+		this.gameManager = new GameManager();
+		
+		this.initializeProperties();
+	}
+	
+	private void initializeProperties() {
+		this.currentLocationDescription = new SimpleStringProperty();
+		this.availableActionsDescriptions = new SimpleStringProperty();
+		this.playerStatus = new SimpleStringProperty();
+		this.availableActions = new SimpleListProperty<>(FXCollections.observableArrayList());
+		
+		this.updateProperties();
+	}
+	
+	private void updateProperties() {
+		this.currentLocationDescription.set(this.gameManager.getCurrentLocation().getDescription());
+		this.playerStatus.set("Health: " + this.gameManager.getPlayer().getHealth());
+		
+		this.updateAvailableActions();
+	}
+	
+	private void updateAvailableActions() {
+		ObservableList<String> actions = FXCollections.observableArrayList();
+		this.gameManager.getCurrentLocation().getActions().forEach(action -> actions.add(action.toString()));
+		this.availableActions.set(actions);
+		
+		StringBuilder formattedActions = new StringBuilder();
+		for (String action : actions) {
+			formattedActions.append(action).append("\n");
+		}
+		this.availableActionsDescriptions.set(formattedActions.toString().trim());
+	}
+	
+	/**
+	 * Handles the action button being pressed
+	 * 
+	 * @param selectedAction	the selected action to be handled
+	 */
+	public void handleActionButtonPressed(String selectedAction) {
+		this.gameManager.getAllAvailableActions().stream()
+				.filter(action -> action.toString().equals(selectedAction))
+				.findFirst()
+				.ifPresent(action -> {
+					this.gameManager.performAction(action);
+				});
+		
+		this.updateProperties();
+	}
+	
+	/**
+	 * Gets the currentLocationDescriptionProperty
+	 * 
+	 * @return	the currentLocationDescriptionProperty
+	 */
+	public StringProperty currentLocationDescriptionProperty() {
+		return this.currentLocationDescription;
+	}
+	
+	/**
+	 * Gets the availableActionsDescriptionsProperty
+	 * 
+	 * @return the availableActionsDescriptionsProperty
+	 */
+	public StringProperty availableActionsDescriptionsProperty() {
+		return this.availableActionsDescriptions;
+    }
+	
+	/**
+	 * Gets the playerStatusProperty
+	 * 
+	 * @return the playerStatusProperty
+	 */
+	public StringProperty playerStatusProperty() {
+		return this.playerStatus;
+    }
+	
+	/**
+	 * Gets the availableActionsProperty
+	 * 
+	 * @return the availableActionsProperty
+	 */
+	public ListProperty<String> availableActionsProperty() {
+		return this.availableActions;
+	}
+}
